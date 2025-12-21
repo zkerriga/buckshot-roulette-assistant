ea29bd660a5ff38795e5d921ea4a2cc7d00b7075
Daniil Eletskii <zkerriga@gmail.com>
2025-12-21 23:21:21 +0100

    add type-classes for relations between dealer and player

--- src/main/scala/com/zkerriga/buckshot/game/events/Used.scala
+++ src/main/scala/com/zkerriga/buckshot/game/events/Used.scala
@@ -1,5 +1,6 @@
 package com.zkerriga.buckshot.game.events
 
+import com.zkerriga.buckshot.game.accessors.{Opposition, Parties}
 import com.zkerriga.buckshot.game.events.Used.ItemUse
 import com.zkerriga.buckshot.game.events.outcome.ErrorMsg.*
 import com.zkerriga.buckshot.game.events.outcome.Outcome.*
@@ -23,32 +24,41 @@ object Used:
     case BurnerPhone(revealed: Shell, at: SeqNr)
     case Meds(good: Boolean)
 
-  private case class WithoutItems(user: Participant, opponent: Participant)
+  private case class Participants(user: Participant, opponent: Participant)
 
   import ItemUse.*
 
   def execute(state: GameState, used: Used): V[GameOver | Reset | GameState] =
     for
       _ <- (state.turnOf == used.actor) trueOr WrongTurn
-      withoutItems <- removeItems(state, used).toRight(MissingItems)
-      outcome <- processEffects(state, withoutItems, used)
+      (opposition, parties) = defineRelations(used)
+      withoutItems <- removeItems(state, used)(using opposition).toRight(MissingItems)
+      outcome <- processEffects(state, withoutItems, used)(using parties)
     yield outcome
 
-  private def removeItems(state: GameState, used: Used): Option[WithoutItems] =
-    val item = itemOf(used.item)
-    def remove(user: Participant, opponent: Participant): Option[WithoutItems] =
-      if used.stolen then
-        for
-          opponent <- opponent.without(item)
-          user <- user.without(Adrenaline)
-        yield WithoutItems(user = user, opponent = opponent)
-      else
-        for user <- user.without(item)
-        yield WithoutItems(user = user, opponent = opponent)
-
+  private def defineRelations(used: Used): (Opposition[GameState], Parties[Participants]) =
     used.actor match
-      case Player => remove(user = state.player, opponent = state.dealer)
-      case Dealer => remove(user = state.dealer, opponent = state.player)
+      case Player =>
+        (
+          Opposition.of(actorIs = _.player, opponentIs = _.dealer),
+          Parties.of(playerIs = _.user, dealerIs = _.opponent),
+        )
+      case Dealer =>
+        (
+          Opposition.of(actorIs = _.dealer, opponentIs = _.player),
+          Parties.of(playerIs = _.opponent, dealerIs = _.user),
+        )
+
+  private def removeItems(state: GameState, used: Used)(using Opposition[GameState]): Option[Participants] =
+    val item = itemOf(used.item)
+    if used.stolen then
+      for
+        opponent <- state.opponent.without(item)
+        user <- state.actor.without(Adrenaline)
+      yield Participants(user = user, opponent = opponent)
+    else
+      for user <- state.actor.without(item)
+      yield Participants(user = user, opponent = state.opponent)
 
   private def itemOf(use: ItemUse): RegularItem = use match
     case Handcuffs => RegularItem.Handcuffs
@@ -60,13 +70,17 @@ object Used:
     case _: BurnerPhone => RegularItem.BurnerPhone
     case _: Meds => RegularItem.Meds
 
-  private def processEffects(state: GameState, itemless: WithoutItems, used: Used): V[GameOver | Reset | GameState] =
+  private def processEffects(
+    state: GameState,
+    itemless: Participants,
+    used: Used,
+  )(using Parties[Participants]): V[GameOver | Reset | GameState] =
     used.item match
       case Handcuffs =>
-        buildNextState(state, user = itemless.user, opponent = itemless.opponent.cuffed).ok
+        buildNextState(state, itemless.copy(opponent = itemless.opponent.cuffed), state.shotgun).ok
 
       case MagnifyingGlass(revealed) =>
-        buildNextState(state, user = itemless.user.knowing(revealed, Shell1), opponent = itemless.opponent).ok
+        buildNextState(state, itemless.copy(user = itemless.user.knowing(revealed, Shell1)), state.shotgun).ok
 
       case Beer(out) =>
         state.shotgun
@@ -76,8 +90,8 @@ object Used:
             case None =>
               Reset.of(
                 maxHealth = state.maxHealth,
-                player = ???,
-                dealer = ???,
+                player = itemless.player,
+                dealer = itemless.dealer,
               )
 
       case Cigarettes => ???
@@ -86,10 +100,15 @@ object Used:
       case BurnerPhone(revealed, at) => ???
       case Meds(good) => ???
 
-  private def buildNextState(state: GameState, user: Participant, opponent: Participant): GameState =
-    state.turnOf match
-      case Player => state.copy(player = user, dealer = opponent)
-      case Dealer => state.copy(dealer = user, player = opponent)
-
-  private def buildNextState(state: GameState, itemless: WithoutItems, shotgun: Shotgun): GameState =
-    buildNextState(state, user = itemless.user, opponent = itemless.opponent).copy(shotgun = shotgun)
+  private def buildNextState(
+    state: GameState,
+    participants: Participants,
+    shotgun: Shotgun,
+  )(using
+    Parties[Participants],
+  ): GameState =
+    state.copy(
+      player = participants.player,
+      dealer = participants.dealer,
+      shotgun = shotgun,
+    )
