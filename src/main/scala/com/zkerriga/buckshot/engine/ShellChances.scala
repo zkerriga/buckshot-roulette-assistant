0762bb4416dff404f75fa4cb744047412131c709
Daniil Eletskii <zkerriga@gmail.com>
2026-01-04 20:32:43 +0100

    fix shell distribution bugs

--- src/main/scala/com/zkerriga/buckshot/engine/ShellChances.scala
+++ src/main/scala/com/zkerriga/buckshot/engine/ShellChances.scala
@@ -2,12 +2,18 @@ package com.zkerriga.buckshot.engine
 
 import com.zkerriga.buckshot.engine.state.{GameState, Revealed}
 import com.zkerriga.buckshot.game.all.*
+import com.zkerriga.buckshot.game.state.shotgun
+import com.zkerriga.types.Nat
 
 object ShellChances {
   def consideringEverything(state: GameState, at: SeqNr): Distribution[Shell] =
-    state.hidden.dealer.belief.getDistribution.flatMap { dealer =>
-      shellAt(state.public.shotgun, state.hidden.player.revealed, dealer, at)
-    }.deduplicate
+    val distribution = state.hidden.dealer.belief.getDistribution
+      .flatMap(dealer => shellAt(state.public.shotgun, state.hidden.player.revealed, dealer, at))
+      .deduplicate
+    at match {
+      case Shell1 => distribution.map(_.considering(state.shotgun.effects))
+      case _ => distribution
+    }
 
   def shellAt(shotgun: Shotgun, player: Revealed, dealer: Revealed, at: SeqNr): Distribution[Shell] =
     val common: Revealed = player.combineWith(dealer)
@@ -16,9 +22,11 @@ object ShellChances {
       case None =>
         shotgun.live
           .minus(common.count(Live))
+          .filterNot(_ == Nat[0])
           .fold(Distribution.deterministic(Blank)): live =>
             shotgun.blank
               .minus(common.count(Blank))
+              .filterNot(_ == Nat[0])
               .fold(Distribution.deterministic(Live)): blank =>
                 Distribution.weighted(
                   live -> Live,
